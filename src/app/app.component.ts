d25db113aae5a622c2432d68e14fdd1ef58e01b5
Amrit Singh <amrit.moomie@comsenseinc.com>
2024-10-16 10:52:58 -0400

    fabric js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Canvas, Rect } from 'fabric'; // browser
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  @ViewChild('canvas', { static: true }) canvasElement: ElementRef<HTMLCanvasElement> | undefined;
   title = 'fabri-js-playground';
+  canvas: Canvas | undefined;
+
+  ngOnInit(): void {
+    if (this.canvasElement) {
+      this.canvas = new Canvas(this.canvasElement.nativeElement);
+      this.addRectangle();
+    }
+  }
+
+  addRectangle(): void {
+    const rect = new Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 200,
+      height: 100,
+    });
+    this.canvas?.add(rect);
+  }
 }
+
+
